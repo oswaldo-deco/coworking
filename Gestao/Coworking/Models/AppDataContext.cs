0156eff8defb65ed9e90c0517f710c885e349726
oswaldo <91742210+oswaldo-deco@users.noreply.github.com>
2024-10-20 18:04:44 -0300

    feature: add AppDataContext to db usage

---
+++ Gestao/Coworking/Models/AppDataContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models;
+
+public class AppDataContext : DbContext
+{
+    public DbSet<Payment> Payment { get; set; }
+    public DbSet<User> User { get; set; }
+    public DbSet<Spaces> Spaces { get; set; }
+    public DbSet<Booking> Booking { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Ecommerce.db");
+    }
+
+}
