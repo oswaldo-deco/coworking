a6ea382a47b6ee5ca36730f0735344275df46422
oswaldo <91742210+oswaldo-deco@users.noreply.github.com>
2024-10-20 18:01:22 -0300

    feature: start payment models

--- Gestao/Coworking/Models/Payment.cs
+++ Gestao/Coworking/Models/Payment.cs
@@ -6,11 +6,12 @@ public class Payment
     {
         Id = Guid.NewGuid().ToString();
         CriadoEm = DateTime.Now;
+        status = 'em processamento'
     }
     public string? Id { get; set; }
-    public string? Nome { get; set; }
-    public string? Descricao { get; set; }
-    public double Valor { get; set; }
-    public int Quantidade { get; set; }
+    public string? bookingId { get; set; }
+    public string? method { get; set; }
+    public double? price { get; set; }
+    public string? status { get; set; }
     public DateTime CriadoEm { get; set; }
 }
--- Gestao/Coworking/Services/PaymentService.cs
+++ Gestao/Coworking/Services/PaymentService.cs
@@ -1,3 +1,33 @@
+using API.Models;
+using API.Repositories;
+
 public class PaymentService {
-    
+    public class PaymentService(){
+    };
+
+    public Payment[] getAll(){
+        return PaymentRepositories.getAll();
+    }
+
+    public Payment create(Payment payment){
+        PaymentRepositories.create(payment);
+
+        return payment;
+    }
+
+    public Payment update(string id){
+        Payment paymentToUpdate = PaymentRepositories.getById(id);
+        for(change){
+            paymentToUpdate.change = change; //corrigir
+        };
+        return PaymentRepositories.updateById(id, payment)
+    }
+
+    public void delete(string id){
+        PaymentRepositories.deleteById(id);
+    }
+
+    public Payment getById(string id){
+        return PaymentRepositories.getById(id);
+    }
 }
