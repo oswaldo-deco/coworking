5aacc1e9bef7c17d13b21d25b1ab27fa8f8f7822
oswaldo <91742210+oswaldo-deco@users.noreply.github.com>
2024-10-20 21:31:47 -0300

    fix: fix code made

--- Gestao/Coworking/Program.cs
+++ Gestao/Coworking/Program.cs
@@ -17,114 +17,195 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/user", () =>
+app.MapGet("/user", ([FromServices] AppDataContext ctx) =>
 {
-    UserService.getAll();
+    if(ctx.Users.Any())
+    {
+        return Results.Ok(ctx.Users.Tolist());
+    }
+    return Results.NotFound();
 });
 
-app.MapGet("/user/:id", () =>
+app.MapGet("/user/:id", ([FromServices] AppDataContext ctx) =>
 {
-    UserService.getById();
+    User? user = ctx.User.Find(id);
+
+    if (user == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(user);
 });
 
-app.UserPost("/user/:id", () =>
+app.UserPost("/user", ([FromServices] AppDataContext ctx) =>
 {
-    UserService.create();
+    ctx.Users.Add(user);
+    ctx.SaveChanges();
+    return Results.Created();
 });
 
-app.MapPut("/users/:id", () =>
+app.MapPut("/users/:id", ([FromServices] AppDataContext ctx) =>
 {
-    UserService.uptade();
+    User? user = ctx.User.Find(id);
+    if (user == null)
+    {
+        return Results.NotFound();
+    }
+    user.Name = userAlterado.Name;
+    user.Email = userAlterado.Email;
+    user.Cellphone = userAlterado.Cellphone;
+    user.TaxNumber = userAlterado.TaxNumber;
+    user.Password = userAlterado.Password;
+    ctx.Users.Update(user);
+    ctx.SaveChanges();
+    return Results.Ok(user);
 });
 
-app.MapDelete("/users/:id", () =>
+app.MapDelete("/users/:id", ([FromServices] AppDataContext ctx) =>
 {
-    UserService.delete();
+    User? user = ctx.User.Find(id);
+    
+    if (user == null)
+    {
+        return Results.NotFound();
+    }
+
+    ctx.Users.Remove(user);
+    ctx.SaveChanges();
+    return Results.Ok(user);
 });
 
-app.MapGet("/spaces", () =>
+app.MapGet("/spaces", ([FromServices] AppDataContext ctx) =>
 {
+    if(ctx.Users.Any())
+    {
+        return Results.Ok(ctx.Spaces.Tolist());
+    }
+    return Results.NotFound();
 });
 
-app.MapGet("/spaces/:id", () =>
+app.MapGet("/spaces/:id", ([FromServices] AppDataContext ctx) =>
 {
+    User? user = ctx.Spaces.Find(id);
+
+    if (user == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(user);
 });
 
-app.MapPost("/spaces/:id", () =>
+app.MapPost("/spaces", ([FromServices] AppDataContext ctx) =>
 {
+    ctx.Spaces.Add(space);
+    ctx.SaveChanges();
+    return Results.Created();
 });
 
-app.MapPut("/spaces/:id", () =>
+app.MapPut("/spaces/:id", ([FromServices] AppDataContext ctx) =>
 {
+    Space? existingSpace = ctx.Space.Find(id);
+    if (user == null)
+    {
+        return Results.NotFound();
+    }
+    existingSpace.Name = space.Name;
+    existingSpace.Capacity = space.Capacity;
+    existingSpace.PricePerHour = space.PricePerHour;
+    ctx.Users.Update(user);
+    ctx.SaveChanges();
+    return Results.Ok(user);
 });
 
-app.MapDelete("/spaces/:id", () =>
+app.MapDelete("/spaces/:id", ([FromServices] AppDataContext ctx) =>
 {
+        Space? spaceToRemove = ctx.Space.Find(id);
+        if (spaceToRemove != null)
+        {
+            _context.Spaces.Remove(spaceToRemove);
+            _context.SaveChanges();
+        }
 });
 
-app.MapGet("/bookings", () =>
+app.MapGet("/bookings", ([FromServices] AppDataContext ctx) =>
 {
-    BookingsService.getAll()
+    if(ctx.Bookings.Any())
+    {
+        return Results.Ok(ctx.Bookings.Tolist());
+    }
+    return Results.NotFound();
 })
 
-app.MapGet("/bookings/:id", () =>
+app.MapGet("/bookings/:id", ([FromServices] AppDataContext ctx) =>
 {
-    BookingsService.getById()
+    Bookings? bookings = ctx.Bookings.Find(id);
+
+    if (bookings == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(bookings);
 })
 
-app.MapPost("/bookings", () =>
+app.MapPost("/bookings", ([FromServices] AppDataContext ctx) =>
 {
-    BookingsService.create()
+    ctx.Bookings.Add(bookings);
+    ctx.SaveChanges();
+    return Results.Created();
 })
 
-app.MapPut("/bookings/:id", () =>
+app.MapPut("/bookings/:id", ([FromServices] AppDataContext ctx) =>
 {
     BookingsService.update()
 })
 
-app.MapDelete("/bookings/:id", () =>
+app.MapDelete("/bookings/:id", ([FromServices] AppDataContext ctx) =>
 {
-    BookingService.delete()
+    Bookings? bookings = ctx.Bookings.Find(id);
+    
+    if (bookings == null)
+    {
+        return Results.NotFound();
+    }
+
+    ctx.Bookings.Remove(bookings);
+    ctx.SaveChanges();
+    return Results.Ok(bookings);
 })
 
-app.MapGet("/payment", () =>
+app.MapGet("/payment", ([FromServices] AppDataContext ctx) =>
 {
     PaymentService.getAll();
 });
 
-app.MapGet("/payment/:id", () =>
+app.MapGet("/payment/:id", ([FromServices] AppDataContext ctx) =>
 {
     PaymentService.getById();
 });
 
-app.MapPost("/payment/:id", () =>
+app.MapPost("/payment/:id", ([FromServices] AppDataContext ctx) =>
 {
     PaymentService.create();
 });
 
-app.MapPut("/payment/:id", () =>
+app.MapPut("/payment/:id", ([FromServices] AppDataContext ctx) =>
 {
     PaymentService.update();
 });
 
-app.MapDelete("/payment/:id", () =>
+app.MapDelete("/payment/:id", ([FromServices] AppDataContext ctx) =>
 {
     PaymentService.delete();
 });
 
-app.MapGet("/kpi", () =>
+app.MapGet("/kpi", ([FromServices] AppDataContext ctx) =>
 {
 });
 
 
-app.MapPost("/login", () =>
+app.MapPost("/login", ([FromServices] AppDataContext ctx) =>
 {
 });
 
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
