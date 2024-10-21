3dc8390a1c9ede19640cc83d254f6ede7269e3ad
YuriTerato <yuri.terato@hotmail.com>
2024-10-20 22:48:17 -0300

    Update: FromRoute

--- Gestao/Coworking/Program.cs
+++ Gestao/Coworking/Program.cs
@@ -37,28 +37,29 @@ app.MapGet("/user/{id}", ([FromServices] AppDataContext ctx, [FromRoute] string 
     return Results.Ok(user);
 });
 
-app.UserPost("/user", ([FromBody] User user) =>
+app.MapPost("/user", ([FromServices] AppDataContext ctx, [FromBody] User user) =>
 {
     ctx.User.Add(user);
     ctx.SaveChanges();
     return Results.Created();
 });
 
-app.MapPut("/User/{id}", ([FromBody] User user) =>
+app.MapPut("/User/{id}", ([FromServices] AppDataContext ctx, [FromBody] User userAlterado, [FromRoute] string id) =>
 {
-    User? user = ctx.User.Find(id);
-    if (user == null)
+    User? userFound = ctx.User.Find(id);
+    
+    if (userFound == null)
     {
         return Results.NotFound();
     }
-    user.Name = userAlterado.Name;
-    user.Email = userAlterado.Email;
-    user.Cellphone = userAlterado.Cellphone;
-    user.TaxNumber = userAlterado.TaxNumber;
-    user.Password = userAlterado.Password;
-    ctx.User.Update(user);
+    userFound.Name = userAlterado.Name;
+    userFound.Email = userAlterado.Email;
+    userFound.Cellphone = userAlterado.Cellphone;
+    userFound.taxNumber = userAlterado.taxNumber;
+    userFound.Password = userAlterado.Password;
+    ctx.User.Update(userFound);
     ctx.SaveChanges();
-    return Results.Ok(user);
+    return Results.Ok(userFound);
 });
 
 app.MapDelete("/User/{id}", ([FromServices] AppDataContext ctx, [FromRoute] string id) =>
@@ -79,131 +80,130 @@ app.MapGet("/spaces", ([FromServices] AppDataContext ctx) =>
 {
     if(ctx.User.Any())
     {
-        return Results.Ok(ctx.Spaces.Tolist());
+        return Results.Ok(ctx.Spaces.ToList());
     }
     return Results.NotFound();
 });
 
-app.MapGet("/spaces/{id}", ([FromServices] AppDataContext ctx, [FromRoute] string id) =>
+app.MapGet("/spaces//{id}", ([FromServices] AppDataContext ctx, [FromRoute] string id) =>
 {
-    User? user = ctx.Spaces.Find(id);
+    Spaces? space = ctx.Spaces.Find(id);
 
-    if (user == null)
+    if (space == null)
     {
         return Results.NotFound();
     }
-    return Results.Ok(user);
+    return Results.Ok(space);
 });
 
-app.MapPost("/spaces", ([FromBody] Spaces space) =>
+app.MapPost("/spaces", ([FromServices] AppDataContext ctx, [FromBody] Spaces space) =>
 {
     ctx.Spaces.Add(space);
     ctx.SaveChanges();
     return Results.Created();
 });
 
-app.MapPut("/spaces/{id}", ([FromBody] Spaces space) =>
+app.MapPut("/spaces/{id}", ([FromServices] AppDataContext ctx, [FromBody] Spaces space, [FromRoute] string id) =>
 {
-    Space? existingSpace = ctx.Space.Find(id);
-    if (user == null)
+    Spaces? existingSpace = ctx.Spaces.Find(id);
+    if (existingSpace == null)
     {
         return Results.NotFound();
     }
     existingSpace.Name = space.Name;
     existingSpace.Capacity = space.Capacity;
     existingSpace.PricePerHour = space.PricePerHour;
-    ctx.User.Update(user);
+    ctx.Spaces.Update(existingSpace);
     ctx.SaveChanges();
-    return Results.Ok(user);
+    return Results.Ok(existingSpace);
 });
 
 app.MapDelete("/spaces/{id}", ([FromServices] AppDataContext ctx, [FromRoute] string id) =>
 {
-        Space? spaceToRemove = ctx.Space.Find(id);
+        Spaces? spaceToRemove = ctx.Spaces.Find(id);
         if (spaceToRemove != null)
         {
-            _context.Spaces.Remove(spaceToRemove);
-            _context.SaveChanges();
+            ctx.Spaces.Remove(spaceToRemove);
+            ctx.SaveChanges();
         }
 });
 
 app.MapGet("/bookings", ([FromServices] AppDataContext ctx) =>
 {
-    if(ctx.Bookings.Any())
+    if(ctx.Booking.Any())
     {
-        return Results.Ok(ctx.Bookings.Tolist());
+        return Results.Ok(ctx.Booking.ToList());
     }
     return Results.NotFound();
 });
 
 app.MapGet("/bookings/{id}", ([FromServices] AppDataContext ctx, [FromRoute] string id) =>
 {
-    Bookings? bookings = ctx.Bookings.Find(id);
+    Booking? booking = ctx.Booking.Find(id);
 
-    if (bookings == null)
+    if (booking == null)
     {
         return Results.NotFound();
     }
-    return Results.Ok(bookings);
+    return Results.Ok(booking);
 });
 
-app.MapPost("/bookings", ([FromBody] Bookings bookings) =>
+app.MapPost("/bookings", ([FromServices] AppDataContext ctx, [FromBody] Booking booking) =>
 {
-    ctx.Bookings.Add(bookings);
+    ctx.Booking.Add(booking);
     ctx.SaveChanges();
     return Results.Created();
 });
 
-app.MapPut("/bookings/{id}", ([FromBody] Bookings bookings) =>
+app.MapPut("/bookings/{id}", ([FromServices] AppDataContext ctx, [FromBody] Booking booking) =>
 {
-    BookingsService.update();
+    Booking.update();
 });
 
 app.MapDelete("/bookings/{id}", ([FromServices] AppDataContext ctx, [FromRoute] string id) =>
 {
-    Bookings? bookings = ctx.Bookings.Find(id);
+    Booking? booking = ctx.Booking.Find(id);
     
-    if (bookings == null)
+    if (booking == null)
     {
         return Results.NotFound();
     }
 
-    ctx.Bookings.Remove(bookings);
+    ctx.Booking.Remove(booking);
     ctx.SaveChanges();
-    return Results.Ok(bookings);
+    return Results.Ok(booking);
 });
 
 app.MapGet("/payment", ([FromServices] AppDataContext ctx) =>
 {
-    PaymentService.getAll();
+
 });
 
 app.MapGet("/payment/{id}", ([FromServices] AppDataContext ctx, [FromRoute] string id) =>
 {
-    PaymentService.getById();
+
+
+app.MapPost("/payment/{id}", ([FromServices] AppDataContext ctx, [FromBody] Payment payment) =>
+{
+
 });
 
-app.MapPost("/payment/{id}", ([FromBody] Payment payment) =>
+app.MapPut("/payment/{id}", ([FromServices] AppDataContext ctx, [FromBody] Payment payment) =>
 {
-    PaymentService.create();
-});
 
-app.MapPut("/payment/{id}", ([FromBody] Payment payment) =>
-{
-    PaymentService.update();
 });
 
 app.MapDelete("/payment/{id}", ([FromServices] AppDataContext ctx, [FromRoute] string id) =>
 {
-    PaymentService.delete();
+
 });
 
-app.MapGet("/kpi", ([FromServices] AppDataContext ctx) =>
+app.MapGet("/kpi", ([FromServices] AppDataContext ctx, [FromServices] AppDataContext ctx) =>
 {
 });
 
 
-app.MapPost("/login", ([FromBody] Login login) =>
+app.MapPost("/login", ([FromServices] AppDataContext ctx, [FromBody] Login login) =>
 {
 });
 
